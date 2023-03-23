a7351af87178ecff95d7ef1faebc649050f07cc9
JoseAngelHdezHdez <joseangel280298@gmail.com>
2023-03-23 10:30:04 -0600

    build:package: creat a new proyect with express and creat the estructur for this proyect and connect to mongoDB

---
+++ database/connectDB.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+try {
+    await mongoose.connect(process.env.URI_MONGO);
+    console.log('Connect DB ok 👍')
+} catch (error) {
+    console.log(`Error de conexion a MongoDB 💔: ${error}`)
+}
---
+++ index.js
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+import './database/connectDB.js'
+import express from 'express';
+import authRouter from './routes/auth.route.js'
+
+const app = express();
+
+app.use(express.json())
+app.use('/api/v1', authRouter)
+
+const PORT = process.env.PORT || 5000;
+app.listen(5000, () => {console.log('👍👍 http://localhost:5000')});
